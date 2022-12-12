eccfbe53b3a69f595fe0322f5eb25cd99a35c73b
MihailSolonin <mkcol1989@gmail.com>
2022-12-12 21:34:51 +0300

    class Horse

---
+++ chees/src/Horse.java
@@ -0,0 +1,13 @@
+public class Horse extends ChessPiece {
+    public Horse(String color) {
+        super(color);
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        
+    }
+}
